05ab2298f496471317f2b6ae85fe934a330c9886
Prathyarthi <prathyarti@gmail.com>
2024-02-12 22:07:44 +0530

    Updated movie routes and tested

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { createMovie, getAllMovies } from '../controllers/movieController.js';
+import { isLoggedIn } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 router.get('/getAllMovies', getAllMovies);
-router.post('/createMovie', createMovie);
+router.post('/createMovie', isLoggedIn, createMovie);
 
 export default router
